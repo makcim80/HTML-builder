5d3d188b467cdd7add99edb39cd361517d229b11
Makcim80 <makcim80@mail.ru>
2023-05-02 00:22:54 +0300

    feat: implement write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdout, stdin, exit } = process;
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Hello!\nEnter your text to write to the file\n');
+
+function endProcess() {
+  stdout.write('Goodbye!\n');
+  exit();
+}
+
+stdin.on('data', data => {
+  if (data.toString() === 'exit\n') {
+    endProcess();
+  } else {
+    output.write(data);
+  }
+});
+
+process.on('SIGINT', () => {
+  stdout.write('\n');
+  endProcess();
+});
